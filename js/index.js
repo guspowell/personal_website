a0329e2cee8684b30c5a5259761851f595216ad1
guspowell <augustuspowell@me.com>
2015-04-23 17:42:48 +0100

    added scrolling js

--- js/index.js
+++ js/index.js
@@ -5,3 +5,37 @@ window.onload =  function fillremaindingPage() {
      var height = "height:" + (windowHeight - navbarHeight) + "px;" + "margin-top:" + navbarHeight + "px;";
      document.getElementById('first-container').setAttribute('style', height)
 }
+
+$(document).ready(function scrolling() {
+
+  $(".about-menu").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".about-container").offset().top
+    }, 1000);
+  });
+
+  $(".skills-menu").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".skills-container").offset().top
+    }, 1000);
+  });
+
+  $(".interests-menu").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".interests-container").offset().top
+    }, 1000);
+  });
+
+  $(".timeline-menu").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".timeline-container").offset().top
+    }, 1000);
+  });
+
+  $(".contact-menu").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".contact-container").offset().top
+    }, 1000);
+  });
+
+});
