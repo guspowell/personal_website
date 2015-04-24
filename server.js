7c0aa6aeb330f9d70c8390bcb8007c099b5ef303
guspowell <augustuspowell@me.com>
2015-04-24 14:51:35 +0100

    changed server.js and package.json in attempt to fix heroku

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
+var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.render('index');
 });
 
-server.listen(3000, function() {
-  console.log('Server listening on port 3000');
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
 
-module.exports = server;
+// module.exports = server;
